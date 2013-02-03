66af6d256cc69473d38e9b588661607d224e3571
Ralph Schuster <github@ralph-schuster.eu>
2013-02-03 13:04:00 +0000

    QOTD initial version as provided by seekay

---
+++ src/main/resources/mysql.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS qotd_settings;
+DROP TABLE IF EXISTS qotd_quotes;
+
+CREATE TABLE qotd_settings (
+	id    INT(11)      UNSIGNED NOT NULL AUTO_INCREMENT,
+	name  VARCHAR(200)          NOT NULL,
+	value VARCHAR(200)          NOT NULL,
+
+	PRIMARY KEY(id)
+) ENGINE=InnoDB;
+
+CREATE TABLE qotd_quotes (
+	id     INT(11)      UNSIGNED NOT NULL AUTO_INCREMENT,
+	quote  VARCHAR(200)          NOT NULL,
+	author VARCHAR(200)          NOT NULL,
+
+	PRIMARY KEY(id)
+) ENGINE=InnoDB;
+
