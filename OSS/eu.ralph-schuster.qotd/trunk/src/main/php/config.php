83b2927cece4db58909daea1d4e0de8cea2dabcd
ralph <ralph@fdcf5c44-6d3c-11dc-8539-f79c8e7a170c>
2013-02-08 22:30:59 +0000

    twitter

--- OSS/eu.ralph-schuster.qotd/trunk/src/main/php/config.php
+++ OSS/eu.ralph-schuster.qotd/trunk/src/main/php/config.php
@@ -5,20 +5,13 @@
  * A single location to store configuration.
  */
 $lines = file('qotd.ini');
-$CONFIG = array();
 
 // Durchgehen des Arrays und Anzeigen des HTML-Quelltexts inkl. Zeilennummern
 foreach ($lines as $line) {
-    echo "Line : " . htmlspecialchars($line) . "<br>\n";
-    $arr = preg_split("/[\s,]+/", $line);
-    $key = $arr[0];
-    $value = $arr[1];
-    $CONFIG[$key] = $value;
-    echo "     " . $key . " == \&gt; " . $value . "<br>\n";
+    	$arr = preg_split("/[\s,]+/", $line);
+	$key = $arr[0];
+	$value = $arr[1];
+	define($key, $value);
 }
 
-define('CONSUMER_KEY', $CONFIG['CONSUMER_KEY']);
-define('CONSUMER_SECRET', $CONFIG['CONSUMER_SECRET']);
-define('ACCESS_TOKEN', $CONFIG['ACCESS_TOKEN']);
-define('ACCESS_TOKEN_SECRET', $CONFIG['ACCESS_TOKEN_SECRET']);
 define('OAUTH_CALLBACK', 'http://qotd.ralph-schuster.eu/callback.php');
---
+++ OSS/eu.ralph-schuster.qotd/trunk/src/main/php/twitter_request_auth.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @file
+ * Check if consumer token is set and if so send user to get a request token.
+ */
+
+/**
+ * Exit with an error message if the CONSUMER_KEY or CONSUMER_SECRET is not defined.
+ */
+require_once('config.php');
+if (CONSUMER_KEY === '' || CONSUMER_SECRET === '') {
+  echo 'You need a consumer key and secret!';
+  exit;
+}
+
+/* Build an image link to start the redirect process. */
+$content = '<a href="./redirect.php"><img src="./images/lighter.png" alt="Sign in with Twitter"/></a>';
+//$content .= "consumer_key=".CONSUMER_KEY." consumer_secret=".CONSUMER_SECRET;
+
+/* Include HTML to display on the page. */
+include('twitter_request_auth.inc');
+
