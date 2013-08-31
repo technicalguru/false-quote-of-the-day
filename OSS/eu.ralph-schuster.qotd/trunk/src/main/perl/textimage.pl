acce77a8ac4c1260382802dcc8e79dde9cd79575
ralph <ralph@fdcf5c44-6d3c-11dc-8539-f79c8e7a170c>
2013-08-31 19:46:35 +0000

    more colors

--- OSS/eu.ralph-schuster.qotd/trunk/src/main/perl/textimage.pl
+++ OSS/eu.ralph-schuster.qotd/trunk/src/main/perl/textimage.pl
@@ -9,9 +9,9 @@ my ($quote, $author, $theme, $num_args);
 $num_args = $#ARGV + 1;
 die "usage: textimage.pl \'quote\' \'author\'\n" unless $num_args == 2;
 
-my @bg = ('ffffff','bf8f30','2a4480','bf5930','259238','70227e');
-my @f1 = ('000000','ffd073','6c8cd5','ff9b73','65e17b','c262d3');
-my @f2 = ('777777','ffbf40','4671d5','ff7640','38e156','bc38d3');
+my @bg = ('ffffff','bf8f30','2a4480','bf5930','259238','70227e','94256d','b0bc2f','206876', '000000','ffd073','6c8cd5','ff9b73','65e17b','c262d3','e266b7','f1fd72','5fbdce');
+my @f1 = ('000000','ffd073','6c8cd5','ff9b73','65e17b','c262d3','e266b7','f1fd72','5fbdce', 'ffffff','bf8f30','2a4480','bf5930','259238','70227e','94256d','b0bc2f','206876');
+my @f2 = ('777777','ffbf40','4671d5','ff7640','38e156','bc38d3','e238a7','edfd3f','37b6ce', 'ffffff','bf8f30','2a4480','bf5930','259238','70227e','94256d','b0bc2f','206876');
 
 $quote = $ARGV[0];
 $author = $ARGV[1];
